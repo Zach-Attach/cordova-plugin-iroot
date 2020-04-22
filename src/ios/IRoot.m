8178761ef8e66a3a3ef0162a25113f35d88115a9
dishant-kaushik <dishant.kaushik@ibm.com>
2020-04-22 13:32:56 -0400

    Removed simulator check

--- src/ios/IRoot.m
+++ src/ios/IRoot.m
@@ -85,9 +85,6 @@ enum {
 
 
 - (bool) jailbroken {
-
-// if !(TARGET_IPHONE_SIMULATOR)
-
     if ([[NSFileManager defaultManager] fileExistsAtPath:@"/Applications/Cydia.app"])
     {
         return YES;
@@ -286,11 +283,6 @@ enum {
         // Jailbroken
         return YES;
     }
-
-
-// endif
-
-    return NO;
 }
 
 
